8fe635f85f7f415b305c9e1f24db16dea0375543
desarrollovolcan <eisla@fvolcan.cl>
2026-01-02 14:07:36 -0300

    Fix cron include paths and add test email flow guard

--- assest/config/menuExpo.php
+++ assest/config/menuExpo.php
@@ -763,9 +763,15 @@ $ARRAYPLANTACAMBIAR = $PLANTA_ADO->listarPlantaPropiaCBX();
           <li>
             <a href="registroUsuarioAviso.php">
               <img src="../../api/cryptioadmin10/html/images/svg-icon/sidebar-menu/miscellaneous.svg" class="svg-icon" alt="">
-              <span>Avisos</span>
+              <span>Registro Aviso</span>
             </a>
-          </li> 
+          </li>
+          <li>
+            <a href="registroUsuarioAviso.php#tab-cron">
+              <img src="../../api/cryptioadmin10/html/images/svg-icon/sidebar-menu/schedule.svg" class="svg-icon" alt="">
+              <span>Configuración Cron PT</span>
+            </a>
+          </li>
         <?php  } ?>
       <?php  } ?>
     </ul>
