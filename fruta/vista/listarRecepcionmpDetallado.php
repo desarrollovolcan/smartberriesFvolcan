6e4dbeb1fcde1d4ed13af063a76da29060fd17de
desarrollovolcan <eisla@fvolcan.cl>
2026-03-04 23:57:22 -0300

    Agregar columna Cuartel en listado de recepción MP detallado

--- fruta/vista/listarRecepcionmpDetallado.php
+++ fruta/vista/listarRecepcionmpDetallado.php
@@ -201,6 +201,7 @@ include_once "../../assest/config/datosUrLP.php";
                                                     <th>Empresa</th>
                                                     <th>Planta</th>
                                                     <th>Temporada</th>
+                                                    <th>Cuartel</th>
                                                 </tr>
                                             </thead>
                                             <tbody>                          
@@ -321,6 +322,13 @@ include_once "../../assest/config/datosUrLP.php";
                                                         }else{
                                                             $NOMBRETTRATAMIENTO1="Sin Datos";
                                                         }
+
+                                                        $ARRAYTRATAMIENTO2 = $TTRATAMIENTO2_ADO->verTtratamiento($s['ID_TTRATAMIENTO2']);
+                                                        if ($ARRAYTRATAMIENTO2) {
+                                                            $NOMBRETTRATAMIENTO2 = $ARRAYTRATAMIENTO2[0]["NOMBRE_TTRATAMIENTO"];
+                                                        } else {
+                                                            $NOMBRETTRATAMIENTO2 = "Sin Datos";
+                                                        }
                                                         ?>
                                                         <tr class="text-center">
                                                             <td><?php echo $s['FOLIO_DRECEPCION']; ?></td>
@@ -351,6 +359,7 @@ include_once "../../assest/config/datosUrLP.php";
                                                             <td><?php echo $NOMBREEMPRESA; ?></td>
                                                             <td><?php echo $NOMBREPLANTA; ?></td>
                                                             <td><?php echo $NOMBRETEMPORADA; ?></td>
+                                                            <td><?php echo $NOMBRETTRATAMIENTO2; ?></td>
                                                         </tr>
                                                     <?php endforeach; ?>
                                                 <?php endforeach; ?>
